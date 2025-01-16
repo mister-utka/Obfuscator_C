d5e164414138510d80d670d78f071d46beb21f20
utka <utka@utka.utka>
2025-01-16 16:04:46 +0700

    Реализовал интерфейс взаимодействия с пользователем после каждого этапа

--- obfuscator.py
+++ obfuscator.py
@@ -12,19 +12,12 @@ dirs_ignore = [".git"]
 
 # определяем список для поиска переменных и функций, данные значения будут использоваться в регулярных выражениях
 data_types_c = ["char", "unsigned char", "signed char",
-                "char*", "unsigned char*", "signed char*",
                 "short", "short int", "signed short", "signed short int", "unsigned short", "unsigned short int",
-                "short*", "short int*", "signed short*", "signed short int*", "unsigned short*", "unsigned short int*",
                 "int", "signed int", "signed", "unsigned int", "unsigned",
-                "int*", "signed int*", "signed*", "unsigned int*", "unsigned*",
                 "long", "long int", "signed long int", "signed long", "unsigned long", "unsigned long int", "long long", "long long int", "signed long long int", "signed long long", "unsigned long long", "unsigned long long int",
-                "long*", "long int*", "signed long int*", "signed long*", "unsigned long*", "unsigned long int*", "long long*", "long long int*", "signed long long int*", "signed long long*", "unsigned long long*", "unsigned long long int*",
                 "float",
-                "float*",
                 "double", "long double",
-                "double*", "long double*",
-                "void", "void*",
-                "size_t", "size_t*"
+                "void", "size_t*",
                 ]
 
 # если в переменную попадут данные значения, они будут игнорироваться
@@ -136,60 +129,139 @@ class Obfuscator():
                 for line in content:
                     for key, data in dict.items():
                         if key in line:
-                            # print(f"key: {key}")
-                            # print(f"line_old: {line.strip()}")
                             if all(case not in line for case in special_cases_substitution_variables):
                                 line = line.replace(key, data)
-                            # print(f"line_new: {line.strip()}\n")
                     file.write(line)
-                    print(line, end="")
                         
         except UnicodeDecodeError:
             pass
 
 
+class Withdrawal():
+
+    def print_inference(self, stage, code_status):
+        if stage == 1:
+            if code_status == 1:
+                print("="*100)
+                print("[*] STAGE 1: Getting all the project files")
+            elif code_status == 2:
+                print("[+] STAGE 1: Is completed")
+                self.getting_data_from_user()
+        elif stage == 2:
+            if code_status == 1:
+                print("="*100)
+                print("[*] STAGE 2: Removing comments and blank lines from files")
+            elif code_status == 2:
+                print("[+] STAGE 2: Is completed")
+                self.getting_data_from_user()
+        elif stage == 3:
+            if code_status == 1:
+                print("="*100)
+                print("[*] STAGE 3: Get all the existing variables and functions in the project")
+            elif code_status == 2:
+                print("[+] STAGE 3: Is completed")
+                self.getting_data_from_user()
+        elif stage == 4:
+            if code_status == 1:
+                print("="*100)
+                print("[*] STAGE 4: Generating obfuscated names ")
+            elif code_status == 2:
+                print("[+] STAGE 4: Is completed")
+                self.getting_data_from_user()
+        elif stage == 5:
+            if code_status == 1:
+                print("="*100)
+                print("[*] STAGE 5: Name substitution ")
+            elif code_status == 2:
+                print("[+] STAGE 5: Is completed")
+
+    def getting_data_from_user(self):
+        while True:
+            user_input = input("Do you want to continue? (yes/no): ")
+            if user_input == "yes" or user_input == "y":
+                break
+            elif user_input == "no" or user_input == "n":
+                exit()
+
+    def print_files(self, list_files):
+        for file in list_files:
+            print(file)
+
+    def print_variables(slef, file, list_variables):
+        # сортируем массив
+        list_variables = sorted(list_variables)
+        # удаляем повторения
+        list_variables = list(dict.fromkeys(list_variables))
+        print("-"*100)
+        print(f"File: {file}")
+        for var in list_variables:
+            if var:
+                print(var)
+
+    def print_obfuscated_variables(slef, dict_obfuscated_variable):
+        # получаем длину самого длинного key в словаре
+        first_key_len = len(max(dict_obfuscated_variable, key=len))
+        for key, data in dict_obfuscated_variable.items():
+            len_key = len(key)
+            # выводим данные в крисивом столбцовом форматировании
+            print(f"{key}" + " "*(int(first_key_len)-int(len_key)) + f" -> {data}")
+
+
 def main():
 
     obfuscator = Obfuscator()
+    withdrawal = Withdrawal()
+
 
     # получение всех файлов в проекте
+    withdrawal.print_inference(stage=1, code_status=1)
     all_files = obfuscator.get_all_files_recursively(path_project)
+    withdrawal.print_files(all_files)
+    withdrawal.print_inference(stage=1, code_status=2)
+
 
     # удаляем пустые строки и коментарии из файлов
+    withdrawal.print_inference(stage=2, code_status=1)
     for file in all_files:
         obfuscator.clearing_blank_lines_and_comments(file)
+    withdrawal.print_inference(stage=2, code_status=2)
     
+
     # получаем все существующие переменные в проекте
+    withdrawal.print_inference(stage=3, code_status=1)
     variables = []
     for file in all_files:
-        print(f"-----------------------------file--------------------------\n{file}")
         vars_file = obfuscator.searching_variables(file)
         if vars_file:
             variables += vars_file
-    # сортируем массив
-    variables = sorted(variables)
-    # удаляем повторения
-    variables = list(dict.fromkeys(variables))
+            withdrawal.print_variables(file, vars_file)
+    # сортируем массив и удаляем повторения
+    variables = list(dict.fromkeys(sorted(variables)))
     # удаляем пустые элементы
     variables = list(filter(None, variables))
-    print(variables)
-    print()
+    withdrawal.print_variables("All found variables and functions:", variables)
+    withdrawal.print_inference(stage=3, code_status=2)
+    
 
+    withdrawal.print_inference(stage=4, code_status=1)
     # получаем словарь {name: random_name} от переданного массива
     modified_names = {}
     obfuscator.name_generation(variables, modified_names)
+    withdrawal.print_obfuscated_variables(modified_names)
     # Сортируем ключи по длине
     # так как если этого не сделать, то маленькие key могут частично заменить код переменной или функции
     modified_names = dict(sorted(modified_names.items(), key=lambda x: len(x[0]), reverse=True))
-    # Создаем новый словарь из отсортированных элементов
-    # modified_names = dict(modified_names)
-    print(modified_names)
+    withdrawal.print_inference(stage=4, code_status=2)
 
+
+    withdrawal.print_inference(stage=5, code_status=1)
     # заменяем имена переменных и функция на новые
     for file in all_files:
-        print(f"\n------------------{file}----------------------\n")
+        print(f"replacement statrt: {file}")
         obfuscator.substitution_variables(file, modified_names)
+        print(f"replacement end:    {file}")
+    withdrawal.print_inference(stage=5, code_status=2)
     
-
+    
 if __name__ == "__main__":
     main()
